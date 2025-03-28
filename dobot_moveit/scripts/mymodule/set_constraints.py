4ded6b2ec5a6d8d9e462daa3c0b5ea44fc228897
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2025-03-28 16:31:55 +0900

    update set_constraints.py

--- dobot_moveit/scripts/mymodule/set_constraints.py
+++ dobot_moveit/scripts/mymodule/set_constraints.py
@@ -3,15 +3,18 @@
 import moveit_commander
 from moveit_msgs.msg import Constraints, JointConstraint
 
-def make_joint_constraint(group_name, limit_angle):
+def make_joint_constraint(group_name, tolerance):
     constraint = Constraints()
     joint_names = group_name.get_active_joints()
+    joint_angles = group_name.get_current_joint_values()
+    i = 0
     for joint_name in joint_names:
         joint_constraint = JointConstraint()
         joint_constraint.joint_name = joint_name
-        joint_constraint.position = 0.0
-        joint_constraint.tolerance_above = limit_angle
-        joint_constraint.tolerance_below = -limit_angle
+        joint_constraint.position = joint_angles[i]
+        joint_constraint.tolerance_above = tolerance
+        joint_constraint.tolerance_below = tolerance
         joint_constraint.weight = 1.0
         constraint.joint_constraints.append(joint_constraint)
+        i += 1
     return constraint
